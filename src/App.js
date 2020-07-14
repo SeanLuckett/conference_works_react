831d00ad8e25c73ad0793c673a5eef4353d45a3d
Sean Luckett <whitecrane007@gmail.com>
2020-07-14 14:39:07 -0600

    Sets up basic nav bar (no links/routing)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import './App.css';
+import classes from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className={classes.App}>
+      <section>
+        <header className={classes.Navbar}>
+          <div className={classes.Logo}>
+            <a href="#"><span>Conference</span>Works</a>
+          </div>
+          <a href="#" className={classes.NavItem}>Schedule</a>
+          <a href="#" className={classes.NavItem}>Speakers</a>
+        </header>
+      </section>
     </div>
   );
 }
