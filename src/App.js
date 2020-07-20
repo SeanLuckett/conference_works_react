f51b5725847f446db2940dffdf6323ab49b02309
Sean Luckett <whitecrane007@gmail.com>
2020-07-20 16:12:42 -0600

    Adds main page text

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React from 'react';
 
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
-import Aux from './higher-order-components/Aux/Aux';
 
 import styles from './App.module.css';
 
 function App() {
   return (
-    <Aux className={styles.App}>
+    <section id="content" className={styles.App}>
       <Header />
+      <section>
+        <p>Welcome to ConferenceWorks: a real website for a fake software conference.</p>
+        <p><a href="#">Register</a> now before this event sells out!</p>
+      </section>
       <Footer />
-    </Aux>
+    </section>
   );
 }
 
