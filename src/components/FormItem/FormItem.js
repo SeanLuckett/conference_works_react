02d1cbfe56a6ae9dd1b6bcabe6d01ab09708ed62
Sean Luckett <whitecrane007@gmail.com>
2020-07-27 15:47:32 -0600

    Adds FormItem validation
    This adds some more dynamism to the FormItem including validation
    messages. NOTE: We chose to rely on simple HTML5 form validation.

--- src/components/FormItem/FormItem.js
+++ src/components/FormItem/FormItem.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './FormItem.module.css';
 
-const formItem = (props) => {
+const FormItem = (props) => {
+  const validMsg = <span className={styles.Valid}> &#10003;</span>;
+  const invalidMsg = <span className={styles.Invalid}> &#10007; {props.validationMsg}</span>;
+
+  const [validationMsg, setValidationMsg] = useState(invalidMsg);
+
+  const isValid = (event) => {
+    if (event.target.validity.valid) {
+      setValidationMsg(validMsg);
+    } else {
+      setValidationMsg(invalidMsg);
+    }
+  }
+
   return (
     <p className={styles.FormItem}>
       <label for={props.inputName}>{props.label} </label>
-      <input name={props.inputName} type={props.inputType} required={props.required} />
-      <span className={styles.Invalid}> &#10007; {props.validationMsg}</span>
-      <span className={styles.Valid}> &#10003;</span>
+      <input
+        name={props.inputName}
+        type={props.inputType}
+        placeholder={props.placeholder}
+        minlength={props.minlength}
+        required={props.required}
+        onChange={(event) => { isValid(event) }} />
+
+      { validationMsg }
     </p>
   );
 };
 
-export default formItem;
+export default FormItem;
--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -8,14 +8,40 @@ const register = () => {
     <section>
       <h1>Register</h1>
       <form action="submit" className={styles.Form}>
-        <FormItem inputName="first-name" label="First name" inputType="text" required={true} validationMsg={"Required"} />
-        <FormItem inputName="last-name" label="Last name" inputType="text" required={true} validationMsg={"Required"}/>
-        <FormItem inputName="email" label="Email" inputType="email" required={true} validationMsg={"Valid email"} />
-        <FormItem inputName="password" label="Password" inputType="password" required={true} validationMsg={"Must be at least 8 characters"} />
-        <formgroup className={styles.ButtonGroup}>
+        <FormItem inputName="first-name" label="First name"
+          inputType="text"
+          placeholder="Jane"
+          required={true}
+          validationMsg={"Required"} />
+
+        <FormItem
+          inputName="last-name"
+          label="Last name"
+          inputType="text"
+          placeholder="Doe"
+          required={true}
+          validationMsg={"Required"} />
+
+        <FormItem
+          inputName="email"
+          label="Email"
+          inputType="email"
+          placeholder="jane.doe@example.com"
+          required={true}
+          validationMsg={"Valid email"} />
+
+        <FormItem
+          inputName="password"
+          label="Password"
+          inputType="password"
+          minlength="8"
+          required={true}
+          validationMsg={"Must be at least 8 characters"} />
+
+        <fieldset className={styles.ButtonGroup}>
           <button className={styles.FormButton} type="submit">Register</button>
           <button className={styles.FormButton} type="reset">Reset</button>
-        </formgroup>
+        </fieldset>
       </form>
     </section>
   );
