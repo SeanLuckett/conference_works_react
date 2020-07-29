413e111c30913bd1ac1a80cbb4832cd7b1688ecb
Sean Luckett <whitecrane007@gmail.com>
2020-07-29 15:16:22 -0600

    Removes all unnecessary code and starts over
    Previous commits were a spike. Now the real fun begins...

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom';
-
-import Homepage from './components/homepage/Homepage';
-import Register from './components/register/Register';
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
-
-import styles from './App.module.css';
 
 function App() {
   return (
-    <section id="content" className={styles.App}>
-      <Header />
-      <Router>
-        <Switch>
-          <Route exact path="/"><Homepage /></Route>
-          <Route path="/register"><Register /></Route>
-        </Switch>
-      </Router>
-      <Footer />
+    <section>
+      Hello, there.
     </section>
   );
 }
