a500b117c320fadc6b573e64c223fae1a97c1d8d
Sean Luckett <whitecrane007@gmail.com>
2020-08-05 15:33:07 -0600

    Adds Header component

---
+++ src/Header/Header.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import Header from './Header';
+
+configure({ adapter: new Adapter() });
+
+describe('<Header />', () => {
+  const component = shallow(<Header />);
+  it('renders logo', () => {
+    expect(component.find('h1').text()).toBe('ConferenceWorks');
+  });
+  
+  it('renders "Schedule" link', () => {
+    expect(component.find('a').at(0).text()).toBe('Schedule');
+  });
+
+  it('renders "Speakers" link', () => {
+    expect(component.find('a').at(1).text()).toBe('Speakers');
+  });
+});
