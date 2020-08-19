b33730f112dbd95cbbd845a408f546239c12515c
Sean Luckett <whitecrane007@gmail.com>
2020-08-19 16:47:49 -0600

    Adds navigation to Schedule page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import styles from './Header.module.css';
 
@@ -7,7 +8,7 @@ const header = () => {
     <header>
       <h1><a href="#"><span className={styles.Logo}>Conference</span>Works</a></h1>
       <nav>
-        <a className={styles.NavItem} href="#">Schedule</a>
+        <Link className={styles.NavItem} to="/schedule">Schedule</Link>
         <a className={styles.NavItem} href="#">Speakers</a>
       </nav>
     </header>
