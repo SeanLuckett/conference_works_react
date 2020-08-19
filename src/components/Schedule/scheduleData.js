a42ea28f8c66caa3914e59f009a7ef1a5f18430b
Sean Luckett <whitecrane007@gmail.com>
2020-08-19 15:19:21 -0600

    Restructures schedule json
    To more easily render the schedule, the json data needed to be
    in a more easily processed structure.

--- src/components/Schedule/scheduleData.js
+++ src/components/Schedule/scheduleData.js
@@ -1,139 +1,63 @@
 export default {
   "sessions": [
     {
-      "rooms": [
-        "gh0",
-        "mh0"
-      ],
-      "talk": "Conference Check-in and Registration",
-      "speaker": null,
+      "talks": ["Conference Check-in and Registration"],
+      "speakers": [],
       "times": ["7:00am", "8:00am"]
     },
     {
-      "rooms": [
-        "gh1"
-      ],
-      "talk": "Agile and Enterprise Architecture are Not Mutually Exclusive",
-      "speaker": "Dr. Rebecca Parsons",
+      "talks": ["Agile and Enterprise Architecture are Not Mutually Exclusive"],
+      "speakers": ["Dr. Rebecca Parsons"],
       "times": ["8:00am", "8:45am"]
     },
     {
-      "rooms": [
-        "gh2",
-        "mh2"
-      ],
-      "talk": "[Transition]",
-      "speaker": null,
+      "talks": ["[Transition]"],
+      "speakers": [],
       "times": ["8:45am", "9:00am"]
     },
     {
-      "rooms": [
-        "gh3"
-      ],
-      "talk": "The Decentralized Web",
-      "speaker": "Brian Sletten",
+      "talks": ["The Decentralized Web", "Microservices Caching Strategies"],
+      "speakers": ["Brian Sletten", "Mark Richards"],
       "times": ["9:00am", "10:30am"]
     },
     {
-      "rooms": [
-        "mh3"
-      ],
-      "talk": "Microservices Caching Strategies",
-      "speaker": "Mark Richards",
-      "times": ["9:00am", "10:30am"]
-    },
-    {
-      "rooms": [
-        "gh4",
-        "mh4"
-      ],
-      "talk": "[Transition]",
-      "speaker": null,
+      "talks": ["[Transition]"],
+      "speakers": [],
       "times": ["10:30am", "10:45am"]
     },
     {
-      "rooms": [
-        "gh5"
-      ],
-      "talk": "Deconstructing the Modern Web Browser",
-      "speaker": "Scott Davis",
-      "times": ["10:45am", "12:15pm"]
-
-    },
-    {
-      "rooms": [
-        "mh5"
-      ],
-      "talk": "Architecture: The Hard Parts",
-      "speaker": "Neal Ford",
+      "talks": ["Deconstructing the Modern Web Browser", "Architecture: The Hard Parts"],
+      "speakers": ["Scott Davis", "Neal Ford"],
       "times": ["10:45am", "12:15pm"]
     },
     {
-      "rooms": [
-        "gh6",
-        "mh6"
-      ],
-      "talk": "Lunch",
-      "speaker": null,
+      "talks": ["Lunch"],
+      "speakers": [],
       "times": ["12:15pm", "1:30pm"]
     },
     {
-      "rooms": [
-        "gh7"
-      ],
-      "talk": "Evolutionary Architecture and Microservices",
-      "speaker": "Dr. Rebecca Parsons",
+      "talks": ["Evolutionary Architecture and Microservices", "Exploring Modern JavaScript"],
+      "speakers": ["Dr. Rebecca Parsons", "Dr. Venkat Subramaniam"],
       "times": ["1:30pm", "3:00pm"]
     },
     {
-      "rooms": [
-        "mh7"
-      ],
-      "talk": "Exploring Modern JavaScript",
-      "speaker": "Dr. Venkat Subramaniam",
-      "times": ["1:30pm", "3:00pm"]
-    },
-    {
-      "rooms": [
-        "gh8",
-        "mh8"
-      ],
-      "talk": "[Transition]",
-      "speaker": null,
+      "talks": ["[Transition]"],
+      "speakers": [],
       "times": ["3:00pm", "3:15pm"]
     },
     {
-      "rooms": [
-        "gh9"
-      ],
-      "talk": "Microservices Migration Patterns",
-      "speaker": "Mark Richards",
+      "talks": ["Microservices Migration Patterns", "Improving Quality of JavaScript Code"],
+      "speakers": ["Mark Richards", "Dr. Venkat Subramaniam"],
       "times": ["3:15pm", "4:45pm"]
     },
     {
-      "rooms": [
-        "mh9"
-      ],
-      "talk": "Improving Quality of JavaScript Code",
-      "speaker": "Dr. Venkat Subramaniam",
-      "times": ["3:15pm", "4:45pm"]
-    },
-    {
-      "rooms": [
-        "gh10",
-        "mh10"
-      ],
-      "talk": "[Transition]",
-      "speaker": null,
+      "talks": ["[Transition]"],
+      "speakers": [],
       "times": ["4:45pm", "5:00pm"]
     },
     {
-      "rooms": [
-        "gh11",
-        "mh11"
-      ],
-      "talk": "It's Spelled 'Accessibility', not 'Disability'",
-      "speaker": "Scott Davis",
+      "talks": ["It's Spelled 'Accessibility', not 'Disability'"],
+      "speakers": ["Scott Davis"],
       "times": ["5:00pm", "6:00pm"]
     }
   ]
